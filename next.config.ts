2d0a715271744aa4931096b878670ea9803a6ea9
VIC <vyahoui@gmail.com>
2026-06-25 23:25:34 +0200

    skip eslint

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {},
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: false },
 };
 
 export default nextConfig;
