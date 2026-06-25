cff8a8ac07ce45b0db71c9f2e84d05808bd620c3
VIC <vyahoui@gmail.com>
2026-06-25 23:00:46 +0200

    fix all ui components

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,12 +1,7 @@
 import * as React from "react"
-
 export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
-
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, ...props }, ref) => {
-    return <button className={className} ref={ref} {...props} />
-  }
-)
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(({ className, ...props }, ref) => {
+  return <button className={className} ref={ref} {...props} />
+})
 Button.displayName = "Button"
-
 export { Button }
--- components/ui/label.tsx
+++ components/ui/label.tsx
@@ -1,20 +1,7 @@
-"use client"
-
 import * as React from "react"
-
-import { cn } from "@/lib/utils"
-
-function Label({ className, ...props }: React.ComponentProps<"label">) {
-  return (
-    <label
-      data-slot="label"
-      className={cn(
-        "flex items-center gap-2 text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50",
-        className
-      )}
-      {...props}
-    />
-  )
-}
-
+export interface LabelProps extends React.LabelHTMLAttributes<HTMLLabelElement> {}
+const Label = React.forwardRef<HTMLLabelElement, LabelProps>(({ className, ...props }, ref) => {
+  return <label className={className} ref={ref} {...props} />
+})
+Label.displayName = "Label"
 export { Label }
